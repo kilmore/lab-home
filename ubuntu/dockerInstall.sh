ab7a1f76b43f69b1d228d5a892357091c80ba46b
kilmore <dan.chernoff@currax.io>
2019-07-12 12:53:14 -0600

    updates to docker install and vagrant file

--- ubuntu/dockerInstall.sh
+++ ubuntu/dockerInstall.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# Install Docker
 sudo apt-get install \
           apt-transport-https \
           ca-certificates \
@@ -16,3 +18,7 @@ sudo apt-get update -y
 sudo apt-get install docker-ce -y
 
 sudo usermod -aG docker vagrant
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
